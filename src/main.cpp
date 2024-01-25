b7c5c588412a42a4705d6d53ed79c9791449a359
vasilis <vasilisalmpanis@gmail.com>
2024-01-25 19:21:55 +0100

    triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
+
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
 
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -12,33 +28,123 @@ void processInput(GLFWwindow* window) {
 
 int main()
 {
+	// Initialize GLFW
 	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using
+	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Tell GLFW we are using the CORE profile
 	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "LearnOpenGL"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
+	
+	// Error check if the window fails to create
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl; 
 		glfwTerminate();
 		return -1;
 	}
+
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
+
+	// Load GLAD so it configures OpenGL
 	gladLoadGL();
-	glViewport(0, 0, 800, 600);
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
-	    while (!glfwWindowShouldClose(window)) {
-		    processInput(window);
+	
+	// Specify the viewport of OpenGL in the window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
 
-		    glClear(GL_COLOR_BUFFER_BIT);
+	// Create and compile the vertex shader and get its reference
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// Attach the vertex shader source code to the vertex shader object
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// Compile the vertex shader into machine code
+	glCompileShader(vertexShader);
 
-		    // Render your scene here
+	// Create fragment shader object and get its reference
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Attach the fragment shader source code to the fragment shader object
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	// Compile the fragment shader into machine code
+	glCompileShader(fragmentShader);
 
-		    glfwSwapBuffers(window);
-		    glfwPollEvents();
-	    }
+	// Create shader program object and get its reference
+	GLuint shaderProgram = glCreateProgram();
+	// Attach vertex and fragment shader to shader program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	// Wrap-up/Link all the shaders together into the shader progr`am
+	glLinkProgram(shaderProgram);
+
+	// Delete the vertex and fragment shaders once linked
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f, // Left  
+		 0.5f, -0.5f, 0.0f, // Right 
+		 0.0f,  0.5f, 0.0f  // Top   
+	};
+	
+	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
+	GLuint VBO, VAO;
+
+	// Generate VAO before VBO
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// Make the VAO the current Vertex Array Object by binding it
+	glBindVertexArray(VAO);
+	
+	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Introduce the vertices into the VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// stream means modiefied once and used many times
+	// static means modiefied once and used many many times
+	// dynamic means modiefied multiple times and used many many times
+
+	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 *sizeof(float), (void *)0);
+
+	// Enable the vertex attribute so that OpenGL knows to use it
+	glEnableVertexAttribArray(0);
+
+	
+	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window)) {
+	    	processInput(window);
+		// Specify the color of the background
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		glUseProgram(shaderProgram);
+		// Bind the VAO so OpenGL knows to use it
+		glBindVertexArray(VAO);
+		// Draw the triangle using the GL_TRIANGLES primitive
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+	// Delete all the objects we've created
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return (EXIT_SUCCESS);
 }
