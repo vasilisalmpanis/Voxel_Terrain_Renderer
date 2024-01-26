ded298412c1c0d8ecf34ef31a4887ddd099fa37a
wrottger <wrottger@student.42wolfsburg.de>
2024-01-26 09:52:58 +0100

    feature: vscode cmake integration

--- build/CMakeFiles/TerrainGenerator.dir/flags.make
+++ build/CMakeFiles/TerrainGenerator.dir/flags.make
@@ -6,5 +6,5 @@ CXX_DEFINES =
 
 CXX_INCLUDES = -I/home/wrottger/Voxel_Terrain_Renderer/include -I/home/wrottger/Voxel_Terrain_Renderer/include/KHR -I/home/wrottger/Voxel_Terrain_Renderer/SYSTEM -I/home/wrottger/Voxel_Terrain_Renderer/glad
 
-CXX_FLAGS = 
+CXX_FLAGS = -g
 
--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Voxel Terrain Renderer", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl; 
 		glfwTerminate();
